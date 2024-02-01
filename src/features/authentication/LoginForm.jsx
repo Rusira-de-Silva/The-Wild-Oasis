8b8bfaa3b76627cd5d66b47ff88c6cac695ee9d9
Rusira de Silva <rusira.21@cse.mrt.ac.lk>
2024-02-01 23:15:09 +0530

    dashboard added

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -2,17 +2,34 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
+import FormRowVerticle from "../../ui/FormRowVertical";
 import Input from "../../ui/Input";
+import { useLogin } from "./useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleSubmit() {}
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address" orientation="vertical">
+      <FormRowVerticle label="Email address" orientation="vertical">
         <Input
           type="email"
           id="email"
@@ -20,19 +37,23 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
-      </FormRow>
-      <FormRow label="Password" orientation="vertical">
+      </FormRowVerticle>
+      <FormRowVerticle label="Password" orientation="vertical">
         <Input
           type="password"
           id="password"
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
-      </FormRow>
+      </FormRowVerticle>
       <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+        <Button disabled={isLoading} size="large">
+          {isLoading ? <SpinnerMini /> : "Log in"}
+        </Button>
       </FormRow>
     </Form>
   );
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://lyioubaqnntpinyrgnne.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+
+export const supabaseUrl = "https://lyioubaqnntpinyrgnne.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5aW91YmFxbm50cGlueXJnbm5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU5MzE4MTUsImV4cCI6MjAyMTUwNzgxNX0.GHnDphHT1jQ49YJ9ZAIrnNGyr_Pm562vIszS6nA0LWY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
